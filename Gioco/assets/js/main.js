14971e7dea10569ab32ae5f99ee8253d6c0e63cd
Quant85 <antonio.85.q@gmail.com>
2020-11-17 18:33:15 +0100

    Aggiunta var valoreGiocatore, valoreBanco

--- Gioco/assets/js/main.js
+++ Gioco/assets/js/main.js
@@ -3,12 +3,16 @@
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 
 var numeroRandomGiocatore;
-var numeroRandomPc;
+var numeroRandomBanco;
 
 var risultatoGiocatore;
-var risultatoPc;
+var risultatoBanco;
 
 var btnLanciaDadi = document.getElementById("btn_lancia");
+
+var valoreGiocatore;
+var valoreBanco;
+var messaggioBanco;
 
 
 btnLanciaDadi.addEventListener("click", function () {
@@ -16,35 +20,36 @@ btnLanciaDadi.addEventListener("click", function () {
   numeroRandomGiocatore = Math.round(Math.random() * (6 - 1) + 1);
   console.log(numeroRandomGiocatore);
   
-  numeroRandomPc = Math.round(Math.random() * (6 - 1) + 1);
-  console.log(numeroRandomPc);
+  numeroRandomBanco = Math.round(Math.random() * (6 - 1) + 1);
+  console.log(numeroRandomBanco);
   
-  if (numeroRandomGiocatore > numeroRandomPc) {
+  if (numeroRandomGiocatore > numeroRandomBanco) {
   
     risultatoGiocatore = "Il tuo lancio ti ha dato un bel " + numeroRandomGiocatore + ". Complimenti hai Vinto!!";
     console.log(risultatoGiocatore);
   
-    risultatoPc = "Che sfotuna un " + numeroRandomPc + ". Per poco mi bastavano solo " + (numeroRandomGiocatore - numeroRandomPc) + " in più e non avresti vinto!! La prossima volta ti batterò.";
-    console.log(risultatoPc);
+    risultatoPc = "Che sfotuna un " + numeroRandomBanco + ". Per poco mi bastavano solo " + (numeroRandomGiocatore - numeroRandomBanco) + " in più e non avresti vinto!! La prossima volta ti batterò.";
+    console.log(risultatoBanco);
   
-  } else if (numeroRandomGiocatore == numeroRandomPc) {
+  } else if (numeroRandomGiocatore == numeroRandomBanco) {
   
     risultatoGiocatore = "Il tuo risultato è " + numeroRandomGiocatore + ". Non basta mi dispiace!!";
     console.log(risultatoGiocatore);
   
-    risultatoPc = "Bella giocata, ma anche io ho un " + numeroRandomPc + ", direi di farne un'altra.";
-    console.log(risultatoPc);
+    risultatoBanco = "Bella giocata, ma anche io ho un " + numeroRandomBanco + ", direi di farne un'altra.";
+    console.log(risultatoBanco);
   } else {
     risultatoGiocatore = "Hai un " + numeroRandomGiocatore + ". Mi dispiace ma questa volta non basta!!";
     console.log(risultatoGiocatore);
   
-    risultatoPc = "Bella giocata, ma il mio " + numeroRandomPc + " non lascia scampo!!";
-    console.log(risultatoPc); 
+    risultatoBanco = "Bella giocata, ma il mio " + numeroRandomBanco + " non lascia scampo!!";
+    console.log(risultatoBanco); 
   }
-  var messaggioBanco;
+
+
 
 messaggioBanco = document.getElementById("messaggio_banco");
-messaggioBanco.innerHTML = risultatoGiocatore + "" + risultatoPc;
+messaggioBanco.innerHTML = risultatoGiocatore + "" + risultatoBanco;
 console.log(messaggioBanco);
 });
 
